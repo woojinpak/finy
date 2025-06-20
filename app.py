2557210cb7ab84214db82ed15fe37c96367f7ec3
woojin <woojin571@gmail.com>
2025-06-20 20:50:30 +0900

    테스트

--- app.py
+++ app.py
@@ -1,102 +1,82 @@
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-import time
-import pandas as ps
-
-# from sqlalchemy import create_engine
-# import pymysql
-
-
-# db = pymysql.connect(host='localhost', user='root', password='1234', charset='utf8')
-# cursor = db.cursor()
-# cursor.execute("USE finy")
-
-# db.commit()
-# db.close()
-
-Name, Price, Platform = [], [], []
-exclude = ['구매', '삽니다', '구해요', '광고']
-
-driver = webdriver.Chrome() #셀레니움 구동
+import pandas as pd
+from sqlalchemy import create_engine
 
 def getBunjang(itemname):
-    url = f"https://m.bunjang.co.kr/search/products?order=date&page=1&q={itemname}"
-    driver.get(url)
+    Name, Price, Platform, Img, Url = [], [], [], [], []
+    exclude = ['광고', '구매', '삽니다', '판매완료']
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
 
+    # 1. 번개장터 크롤링
+    url = f"https://m.bunjang.co.kr/search/products?order=date&q={itemname}&page=1"
+    driver.get(url)
     i = 1
     while True:
         try:
             nameInfo = driver.find_element(By.XPATH, f'//*[@id="root"]/div/div/div[4]/div/div[4]/div/div[{i}]/a/div[2]/div[1]').text
             price = driver.find_element(By.XPATH, f'//*[@id="root"]/div/div/div[4]/div/div[4]/div/div[{i}]/a/div[2]/div[2]/div[1]').text
-        
-        except NoSuchElementException: #없으면 정지
+            img = driver.find_element(By.XPATH, f'//*[@id="root"]/div/div/div[4]/div/div[4]/div/div[{i}]/a/div[1]/img').get_attribute('src')
+            url_link = driver.find_element(By.XPATH, f'//*[@id="root"]/div/div/div[4]/div/div[4]/div/div[{i}]/a').get_attribute('href')
+        except NoSuchElementException:
             break
-
-        if any(keyword in nameInfo for keyword in exclude): # 제외 키워드가 있으면 건너뛰기
+        if 'ads' in img or any(keyword in nameInfo for keyword in exclude):
             i += 1
             continue
-
-        price_clean = price.replace(',', '').strip() #가격에서 쉼표, 공백 제거
         Name.append(nameInfo)
-        Price.append(price_clean)
-        Platform.append('Bunjang')
+        Price.append(price.replace(',', '').strip())
+        Platform.append('번개장터')
+        Img.append(img)
+        Url.append(url_link)
         i += 1
 
-        
-
-def getFruits(itemname):
+    # 2. 후르츠패밀리 크롤링
     url = f'https://fruitsfamily.com/search/{itemname}'
     driver.get(url)
-
     i = 1
     while True:
         try:
-            nameInfo = driver.find_element(By.XPATH, f'//*[@id="root"]/div[1]/section/div/div/div/div/div/ul/li[{i}]/div/div/h7').text
+            nameInfo = driver.find_element(By.XPATH, f'//*[@id="root"]/div[1]/section/div/div/div/div/div/ul/li[{i}]/div/div/p').text
             price = driver.find_element(By.CSS_SELECTOR, f'#root > div.App-content > section > div > div > div > div > div > ul > li:nth-child({i}) > div > div > div.ProductsListItem-price-container > div.ProductsListItem-price.font-proxima').text
-
+            img = driver.find_element(By.XPATH, f'//*[@id="root"]/div[1]/section/div/div/div/div/div/ul/li[{i}]/div/div/div[1]/div/a/img').get_attribute('src')
+            url_link = driver.find_element(By.XPATH, f'//*[@id="root"]/div[1]/section/div/div/div/div/div/ul/li[{i}]/div/div/div[1]/div/a').get_attribute('href')
         except NoSuchElementException:
             break
-
+        if any(keyword in nameInfo for keyword in exclude):
+            i += 1
+            continue
         price_clean = int(price.replace('원', '').replace(',', '').strip())
         Name.append(nameInfo)
         Price.append(price_clean)
-        Platform.append('FruitsFamily')
+        Platform.append('후르츠패밀리')
+        Img.append(img)
+        Url.append(url_link)
         i += 1
 
-def getJoongna(itemname):
-    url = f'https://web.joongna.com/search/{itemname}?sort=RECENT_SORT'
-    driver.get(url)
+    driver.quit()
+    return Name, Price, Platform, Img, Url
 
-    i=1
-    while True:
-        try:
-            nameInfo = driver.find_element(By.XPATH, f'//*[@id="__next"]/div/main/div[1]/div/ul[2]/li[{i}]/a/div[2]/h2').text
-            price = driver.find_element(By.XPATH, f'//*[@id="__next"]/div/main/div[1]/div/ul[2]/li[{i}]/a/div[2]/div[1]').text
-        except NoSuchElementException:
-            break
+if __name__ == '__main__':
+    itemname = sys.argv[1]
 
-        price_clean = int(price.replace('원', '').replace(',', '').strip())
-        Name.append(nameInfo)
-        Price.append(price_clean)
-        Platform.append('Joongna')
-        i += 1
+    Name, Price, Platform, Img, Url = getBunjang(itemname)
 
-
-#검색어 입력
-itemname = input("검색어 입력 : ")
-
-#번장 가져오기
-getBunjang(itemname)
-#getFruits(itemname)
-getJoongna(itemname)
-
-df = ps.DataFrame({
-    'Name' : Name,
-    'Price' : Price,
-    'Platform' : Platform
-})
-
-df.to_csv('products.csv', index=False)
-
-print(df)
+    data = {
+        'Name': Name,
+        'Price': Price,
+        'Platform': Platform,
+        'Img': Img,
+        'Url': Url
+    }
+    df = pd.DataFrame(data)
+    engine = create_engine("mysql+pymysql://root:1234@localhost:3306/finy")
+    db_imgs = pd.read_sql("SELECT Img FROM item", con=engine)
+    db_img_set = set(db_imgs['Img'])
+    df = df[~df['Img'].isin(db_img_set)]
+    df = df[~df['Img'].astype(str).str.contains('ads')]
+    if not df.empty:
+        df.to_sql('item', con=engine, if_exists='append', index=False)
--- server.py
+++ server.py
@@ -1,19 +1,28 @@
-from flask import Flask
+from flask import Flask, request, render_template, jsonify, send_from_directory
+import pandas as pd
+from sqlalchemy import create_engine
+import subprocess
 
 app = Flask(__name__)
 
+@app.route('/run_crawl')
+def run_crawl():
+    name = request.args.get('name', '')
+    subprocess.run(['python', 'app.py', name], check=True)
+    return jsonify({'status': 'done'})
+
+@app.route('/search')
+def search():
+    name = request.args.get('name', '')
+    engine = create_engine("mysql+pymysql://root:1234@localhost:3306/finy")
+    # f-string으로 쿼리문 만들기 (params 사용 X)
+    query = f"SELECT * FROM item WHERE Name LIKE '%%{name}%%'"
+    df = pd.read_sql(query, con=engine)
+    return render_template('search.html', items=df.to_dict(orient='records'), keyword=name)
+
 @app.route('/')
 def index():
-    return '''<!doctype html>
-    <html>
-        <body>
-            hello
-        </body>
-    </html>
+    return send_from_directory('.', 'index.html')
 
-
-
-
-    '''
-
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
